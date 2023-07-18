d1ce3b8569ac8a95dcfce596db349a37bdc01eee
lucaschier <ens-lucasschier@ugv.edu.br>
2023-07-18 14:48:06 -0300

    Desafio: Criar componente de texto

---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface TextProps {
+  tag?: 'h1' | 'h2' | 'h3' | 'h4' | 'p';
+  children: React.ReactNode;
+}
+
+export default function Text({ tag, children }: TextProps) {
+  const Tag = tag || "p";
+  return (
+    <Tag>
+      {children}
+    </Tag>
+  );
+}
+
+Text.defaultProps = {
+  tag: "p",
+}
