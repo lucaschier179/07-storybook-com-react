1ccddadf489870b1e40417e25392c227e8da90dd
lucaschier <ens-lucasschier@ugv.edu.br>
2023-07-19 09:00:19 -0300

    Addons e aprimoramentos para a documentação

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -9,6 +9,7 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
+    "addon-screen-reader"
   ],
   framework: {
     name: "@storybook/nextjs",
